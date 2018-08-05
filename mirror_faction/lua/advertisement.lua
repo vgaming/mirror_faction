97793905285e13129bff1edf956247a0f74749a0
Vasya N <n1m5spm@yandex.com>
2018-08-05 21:17:10 +0300

    remove duplication

--- mirror_faction/lua/advertisement.lua
+++ mirror_faction/lua/advertisement.lua
@@ -5,17 +5,19 @@ local tostring = tostring
 
 local script_arguments = ...
 local remote_version = tostring(script_arguments.remote_version)
+local filename = "~add-ons/mirror_faction/target/version.txt"
 
-if not wesnoth.have_file("~add-ons/mirror_faction") then
+if not wesnoth.have_file(filename) then
 	wesnoth.message("", "Factions are mirrored using 'Mirror Faction' add-on. If you like it, feel free to download it.")
 else
-	local local_version = wesnoth.read_file("~add-ons/mirror_faction/target/version.txt")
+	local local_version = wesnoth.read_file(filename)
 	if wesnoth.compare_versions(remote_version, ">", local_version) then
 		wesnoth.wml_actions.message {
 			caption = "Mirror Faction",
-			message = [[🠉🠉🠉 Please upgrade your Mirror Faction add-on 🠉🠉🠉
-
-(You can do that after the game)]],
+			message = "🠉🠉🠉 Please upgrade your Mirror Faction add-on 🠉🠉🠉"
+				.. "\n\n"
+				.. local_version .. " -> " .. remote_version
+				.. "(You can do that after the game)",
 			image = "misc/blank-hex.png~BLIT(icons/unit-groups/race_drake_30.png,5,6)~BLIT(icons/unit-groups/race_wose_30.png,9,36)~BLIT(icons/unit-groups/race_drake_30.png~FL(),40,6)~BLIT(icons/unit-groups/race_wose_30.png~FL(),36,36)",
 		}
 	end
