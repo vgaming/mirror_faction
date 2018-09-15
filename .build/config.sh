f5381a99d22170b2b169ef3a82522251d9934a57
Vasya N <n1m5spm@yandex.com>
2018-09-15 11:52:17 +0300

    use sync advertisement

--- .build/config.sh
+++ .build/config.sh
@@ -11,3 +11,5 @@ upload_to_wesnoth_versions=(1.14)
 #	echo "$hex~BLIT($drake,6,6)~BLIT($wose,9,36)~BLIT($drake2,39,6)~BLIT($wose2,36,36)"
 #}
 #addon_manager_args=("--pbl-key" "icon" "$(icon)")
+
+addon_manager_args=("--pbl-key" "icon" "$(cat src/doc/icon.txt)")
